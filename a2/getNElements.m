abeb13ce875063ef5eb601df64d04caaf8b86474
Colin Stone <colin@bunnylaundering.com>
2015-09-23 22:24:27 -0700

    add source

--- a2/getNElements.m
+++ a2/getNElements.m
@@ -1,3 +1,4 @@
+%source: http://stackoverflow.com/questions/14140746/how-to-find-the-index-of-the-n-smallest-elements-in-a-vector
 function [smallestNElements smallestNIdx] = getNElements(A, n)
      [ASorted AIdx] = sort(A);
      smallestNElements = ASorted(1:n);
