dae80d9b422319c628c8ea0d5f853d30fb6c5bfd
Colin Stone <colin@bunnylaundering.com>
2015-10-11 13:58:04 -0700

    1.2 DBSCAN for animals

--- a3/example_animals.m
+++ a3/example_animals.m
@@ -1,10 +1,12 @@
 %% Animals with attributes data
 load animals.mat
 
-%% K-Means clustering
-K = 5;
-model = clusterKmeans(X,K);
+%% Density-Based Clustering
+eps = 13;
+minPts = 3;
+model = clusterDBcluster(X,eps,minPts);
 
+%% Print Clusters
 for k = 1:max(model.clusters)
     fprintf('Cluster %d: ',k);
     fprintf('%s ',animals{model.clusters==k});
