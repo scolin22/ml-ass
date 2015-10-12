724b716253e222a9ddf36ad39ededf51efbbc347
Colin Stone <colin@bunnylaundering.com>
2015-10-11 17:50:24 -0700

    1.3 UBClustering

--- a3/clusterUBClustering.m
+++ a3/clusterUBClustering.m
@@ -1,5 +1,4 @@
 function [model] = clusterUBClustering(X,K,nModels)
-
 [N,D] = size(X);
 
 for m = 1:nModels
@@ -9,17 +8,76 @@ end
 for m = 1:nModels
     clusters(:,m) = model.subModel{m}.predict(model.subModel{m},X);
 end
-clusters = mode(clusters,2);
-model.clusters = clusters;
 
-% If we only have two features, make a colored scatterplot
-if D == 2
-    clf;hold on;
-    colors = getColorsRGB;
+% New Code
+% Store likelihood of pairs appearing in the same cluster
+pairProb = zeros(N, N);
+
+for m = 1:nModels
     for k = 1:K
-        plot(X(clusters==k,1),X(clusters==k,2),'o','Color',.75*colors(k,:),'MarkerSize',5,'MarkerFaceColor',.75*colors(k,:));
+        same = clusters(:, m) == k;
+        idx = find(same);
+
+        % Sum new occurences of nodes appearing the same cluster
+        pairProb(:, idx) = pairProb(:, idx) + repmat(same, size(idx'));
+    end
+end
+
+% Calculate probabilities
+pairProb = pairProb * 1/nModels;
+
+% Cluster based on probabilities of pairs appearing in the same cluster > 0.5
+clusters = clusterProbBasedcluster(X, 0.5, 1, pairProb);
+% End New Code
+
+model.clusters = clusters;
+end
+
+%% Probablistic Based Clustering
+function [cluster] = clusterProbBasedcluster(X,eps,minPts,D)
+[N,~] = size(X);
+
+% This will be the cluster of each object.
+cluster = zeros(N,1);
+
+% This variable will keep track of whether we've visited each object.
+visited = zeros(N,1);
+
+% K will count the number of clusters we've found
+K = 0;
+for i = 1:N
+    if ~visited(i)
+        % We only need to consider examples that have never been visited
+        visited(i) = 1;
+        % Take likelihoods > eps
+        neighbors = find(D(:,i) > eps);
+        if length(neighbors) >= minPts
+            % We found a new cluster
+            K = K + 1;
+            [visited,cluster] = expand(X,i,neighbors,K,eps,minPts,D,visited,cluster);
+        end
     end
 end
 end
 
+function [visited,cluster] = expand(X,i,neighbors,K,eps,minPts,D,visited,cluster)
+cluster(i) = K;
+ind = 0;
+while 1
+    ind = ind+1;
+    if ind > length(neighbors)
+        break;
+    end
+    n = neighbors(ind);
+    cluster(n) = K;
 
+    if ~visited(n)
+        visited(n) = 1;
+        % Take likelihoods > eps
+        neighbors2 = find(D(:,n) > eps);
+        if length(neighbors2) >= minPts
+            neighbors = [neighbors;setdiff(neighbors2,neighbors)];
+        end
+    end
+end
+end
