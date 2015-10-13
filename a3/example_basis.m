cc366d3db638b789e6775e94122db7a70e7164a4
Colin Stone <colin@bunnylaundering.com>
2015-10-12 21:48:12 -0700

    3.3 choosing basis

--- a3/example_basis.m
+++ a3/example_basis.m
@@ -2,21 +2,38 @@
 % Load data
 load basisData.mat
 
-degree = 8;
-figure(1);
+[N,~] = size(X);
+
+Ntrain = N / 2;
+
+Xtrain      = X(1:Ntrain,:);
+Xvalidation = X(Ntrain+1:end,:);
+ytrain      = y(1:Ntrain,:);
+yvalidation = y(Ntrain+1:end,:);
+
+degree = 20;
+
+trainMSE = zeros(degree,1);
+validationMSE = zeros(degree,1);
 
 for d = 0:degree
-    % Plot data
-    subplot(3,3,d+1)
-    plot(X,y,'b.')
-    title(['degree = ', num2str(d)]);
-    hold on
-
     % Fit least-squares estimator
-    model = leastSquaresBasis(X,y,d);
+    model = leastSquaresBasis(Xtrain,ytrain,d);
 
-    % Draw model prediction
-    Xsample = [min(X):.1:max(X)]';
-    yHat = model.predict(model,Xsample);
-    plot(Xsample,yHat,'g-');
+    % Testing Error
+    yhat = model.predict(model,Xtrain);
+    trainMSE(d+1) = sum((yhat - ytrain).^2)/Ntrain;
+
+    % Validation Error
+    yhat = model.predict(model,Xvalidation);
+    validationMSE(d+1) = sum((yhat - yvalidation).^2)/Ntrain;
+
+    fprintf('degree %02d: , test error: %f, validation error: %f\n',d,trainMSE(d+1),validationMSE(d+1));
 end
+
+% subplot(2,1,1);
+% plot(0:degree, trainMSE, '-ro');
+% legend('trainMSE');
+% subplot(2,1,2);
+plot(0:degree, abs(trainMSE - validationMSE)./trainMSE, '-ro');
+legend('(trainMSE - validationMSE) / trainMSE');
