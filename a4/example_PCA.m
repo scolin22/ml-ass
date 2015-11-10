61cc022f53d367efb43311a150db61829b2048c8
Colin Stone <colin@bunnylaundering.com>
2015-11-10 00:38:27 -0800

    2.1

--- a4/example_PCA.m
+++ a4/example_PCA.m
@@ -1,10 +1,31 @@
 load animals.mat
 
 [n,d] = size(X);
+k = 2;
 X = standardizeCols(X);
 
-figure(1);
-imagesc(X);
-figure(2);
-plot(X(:,1),X(:,2),'.');
-gname(animals);
+[U,S,V] = svd(X);
+W = V(1:k,:);
+% Check L2 norm of each row is 1
+% and Inner Product between first and all other rows is 0
+for row = 1:k
+    if norm(W(k,:)) - 1 > 1e-10
+        fprintf('Scaling issue\n');
+    end
+    if W(1,:)*W(k,:)' > 1e-10
+        fprintf('Orthogonality issue\n');
+    end
+end
+
+Z = X*W';
+
+% figure(1);
+% imagesc(Z);
+hFig = figure(2);
+set(hFig, 'Position', [0 0 800 1024])
+plot(Z(:,1),Z(:,2),'.');
+% gname(animals);
+hold on;
+for i = 1:n
+    text(Z(i,1),Z(i,2),animals(i,:));
+end
